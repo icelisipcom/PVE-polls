73be73b8c7652fba057a68613f39f83100cdf160
fgonzalez <felmiquiztli@gmail.com>
2023-04-13 10:53:34 -0600

    primer conexion eloquent

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\respuestas2;
 class HomeController extends Controller
 {
     /**
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-       
-        return view('home');
+        $encuestas=respuestas2::all();
+        return view('home',compact('encuestas'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,29 @@
 @section('content')
     <div class="container-fluid">
         <h1 class="text-black-50">You are logged in!</h1>
+        <div class="col-6 col-sm-12 table-responsive">
+                <table class="table  table-striped text-xs font-medium">
+                    <thead>
+                        <tr class="text-center">
+                            <th>Encuestador</th>
+                            <th>FECHA captura</th>
+                            <th>carrera</th>
+                            <th>plantel </th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($encuestas as $e)
+                        <tr>
+                         <td> {{$e->aplica}}</td>
+                         <td> {{$e->fec_capt}}</td>
+                         <td> {{$e->nbr2}}</td>
+                         
+                         <td> {{$e->nbr3}}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+        </div>
     </div>
 @endsection
