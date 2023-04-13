ac65812af490927e9cc571ac8dff81282679782f
fgonzalez <felmiquiztli@gmail.com>
2023-04-13 09:58:30 -0600

    first ocmmit

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,13 @@
+<!-- need to remove -->
+<li class="nav-item">
+    <a href="{{ route('home') }}" class="nav-link {{ Request::is('home') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-home"></i>
+        <p>Home</p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="{{ route('home') }}" class="nav-link {{ Request::is('home') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-eye"></i>
+        <p>Home</p>
+    </a>
+</li>
