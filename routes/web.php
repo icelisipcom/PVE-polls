f79dd53a0c5598a43ca13545d47d434da0c58505
fgonzalez <felmiquiztli@gmail.com>
2023-04-24 10:08:12 -0600

    cds

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-
+Route::get('/', function () {
+    return redirect(route('login'));
+});
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
