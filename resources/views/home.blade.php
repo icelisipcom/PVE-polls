8f56d33d0391602d1b07f4d39322ecc9a1d523fb
fgonzalez <felmiquiztli@gmail.com>
2023-04-25 09:52:49 -0600

    agregando el modelo de conezxion respuestas2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container-fluid">
-        <h1 class="text-black-50">Estado del Estudio</h1>
+        <h1 class="text-black-50">Estado del Estudio 2019</h1>
         <div class="col-6 col-sm-12 table-responsive">
                 <table class="table  table-striped text-xs font-medium">
                     <thead>
