fb30e5e4f3b1bff49f9e9b5e5f0eb71ccc6dd89a
fgonzalez <felmiquiztli@gmail.com>
2023-05-16 13:37:54 -0600

    fds

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 //nmms no esta cambiando
 
-
+Route::get('/', function () {
+    return redirect(route('login'));
+});
 Route::get('home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+Auth::routes();
 
