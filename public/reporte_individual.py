f67a8aaad8cf7e8b928a33adbda6095386360d52
fgonzalez <felmiquiztli@gmail.com>
2023-06-15 11:19:23 -0600

    dsg

--- public/reporte_individual.py
+++ public/reporte_individual.py
@@ -13,12 +13,12 @@ DB_USERNAME = os.getenv('DB_USERNAME')
 DB_DATABASE = os.getenv('DB_DATABASE')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_PORT = os.getenv('DB_PORT')
-
+DB_HOST=os.getenv('DB_HOST')
 
 # Conectar a DB
 cnx = mysql.connector.connect(user=DB_USERNAME,
                               password=DB_PASSWORD,
-                              host='localhost',
+                              host=DB_HOST,
                               port=DB_PORT,
                               database=DB_DATABASE,
                               use_pure=False)
