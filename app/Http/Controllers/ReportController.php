cdb93530f00540dd735a2f87539afb6bf59c028a
fgonzalez <felmiquiztli@gmail.com>
2023-06-15 13:52:28 -0600

    reporte2

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Symfony\Component\Process\Process; 
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
 class ReportController extends Controller
 {
     public function rep_individual()
