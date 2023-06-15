359ac2ed92fe98709ff784e3fd91f22f02d8c13e
fgonzalez <felmiquiztli@gmail.com>
2023-06-15 13:39:02 -0600

    reporte

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+    public function rep_individual()
+       {
+           $caminoalpoder=public_path();
+           $process = new Process(['python3',$caminoalpoder.'/reporte_individual.py']);
+           $process->run();
+           if (!$process->isSuccessful()) {
+               throw new ProcessFailedException($process);
+           }
+           $data = $process->getOutput();
+           
+               return response()->download(public_path('storage/reporte_individual.xlsx'));
+          
+       }}
+}
