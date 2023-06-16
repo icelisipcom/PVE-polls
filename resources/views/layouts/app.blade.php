7f6b0abe01c8cb4e3a3e449c457bcfb48aa2e1d5
fgonzalez <felmiquiztli@gmail.com>
2023-06-16 10:49:37 -0600

    asf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,9 @@
 }
      </style>
      @stack('css')
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
     <body class="hold-transition sidebar-mini layout-fixed"  >
         <div class="wrapper">
