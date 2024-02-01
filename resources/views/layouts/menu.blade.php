c98fb1117fce98edb0261f25fd9bfdf7d4d4f953
fgonzalez <felmiquiztli@gmail.com>
2024-02-01 13:59:23 -0600

    adgadg

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -6,24 +6,14 @@
         <p>Dashboard</p>
     </a>
 </li>
-<li class="nav-item">
-    <a href="{{ route('2020') }}" class="nav-link {{ Request::is('2020') ? 'active' : '' }}">
-    <i class="fas fa-poll"></i>
-        <p> Encuesta 2019 </p>
-    </a>
-</li>
+
 <li class="nav-item">
     <a href="{{ route('2014_act') }}" class="nav-link {{ Request::is('2014_act') ? 'active' : '' }}">
     <i class="fas fa-poll"></i>
         <p>Actualizacion 2014 </p>
     </a>
 </li>
-<li class="nav-item">
-    <a href="{{ route('2019') }}" class="nav-link {{ Request::is('2019') ? 'active' : '' }}">
-    <i class="fas fa-poll"></i>
-        <p> Encuesta 2019 </p>
-    </a>
-</li>
+
 <li class="nav-item">
     <a href="{{ route('muestras.index') }}" class="nav-link {{ Request::is('muestras') ? 'active' : '' }}">
     <i class="nav-icon fas fa-table"></i>
