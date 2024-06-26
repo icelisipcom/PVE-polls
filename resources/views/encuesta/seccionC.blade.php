850420e9ca45209bfeaec6ffaf7eb3681d1e61b7
fgonzalez <felmiquiztli@gmail.com>
2024-06-26 15:45:56 -0600

    dsg

--- resources/views/encuesta/seccionC.blade.php
+++ resources/views/encuesta/seccionC.blade.php
@@ -75,7 +75,7 @@
 <h2 class="reactivo">63a.- Extranjero Especifique: </h2>
 
 
- <INPUT type="text" class="texto " id="ncr2ext" name="ncr2ext" value="{{$Encuesta->ncr2text}}" style="width:60%"  maxlength="110"  >
+ <INPUT type="text" class="texto " id="ncr2ext" name="ncr2ext" value="{{$Encuesta->ncr2ext}}" style="width:60%"  maxlength="110"  >
 
 
 </td>
@@ -499,7 +499,9 @@ function seccionc2(){
       reactivosPorCerrar.forEach(ocultar);
     break;
     case '7':
-      reactivosPorCerrar=[ncr3,ncr4,ncr5,ncr4a,ncr6a,ncr6otra,ncr6a2,ncr7a,ncr7b,ncr8,ncr9,ncr10,ncr12_a,ncr15,ncr16,ncr17,ncr18,ncr19,ncr20,ncr22,ncr24,ncr24a,ncr24porque,ncr23];
+      reactivosPorAbrir=[ncr2,ncr2a,ncr2ext,ncr3,ncr4,ncr5,ncr4a,ncr6a,ncr6otra,ncr6a2,ncr7a,ncr7b,ncr8,ncr9,ncr10,ncr11,ncr12_a,ncr15,ncr16,ncr17,ncr18,ncr19,ncr20,ncr21_a,ncr22];
+      reactivosPorAbrir.forEach(visibilizar);
+      reactivosPorCerrar=[ncr3,ncr4,ncr5,ncr4a,ncr6a,ncr6otra,ncr6a2,ncr7a,ncr7b,ncr8,ncr9,ncr10,ncr12_a,ncr15,ncr17,ncr18,ncr19,ncr20,ncr22,ncr24,ncr24a,ncr24porque,ncr23];
       reactivosPorCerrar.forEach(ocultar);
     break;
     case '8':
@@ -547,11 +549,11 @@ bloquear('ncr4',[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22,23],[ncr4
 autoempleo();
 bloquear('ncr8',[0,2],[ncr9]); 
 porque();
-bloquear('ncr22',[2],[ncr24,ncr24a,ncr24porque,ncr23])
+bloquear('ncr22',[2,0],[ncr24,ncr24a,ncr24porque,ncr23])
 seccionc2();
 bloquear('ncr4',[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22,23],[ncr4a])
 bloquear('ncr2a',[0,1,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33],[ncr2ext])
-bloquear('ncr22',[2],[ncr24,ncr24a,ncr24porque,ncr23])
+bloquear('ncr22',[2,0],[ncr24,ncr24a,ncr24porque,ncr23])
 autoempleo();
 func_ncr11();
 @if($Encuesta->ncr1==1 |$Encuesta->ncr1==2)
